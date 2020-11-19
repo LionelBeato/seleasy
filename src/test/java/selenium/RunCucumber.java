0825f838d0a1f3325b22c5c8b6012b743e7a9810
LionelBeato <lionel.beato@gmail.com>
2020-11-19 13:37:02 -0500

    small tinkering

--- src/test/java/selenium/RunCucumber.java
+++ src/test/java/selenium/RunCucumber.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "classpath:features", plugin = {"pretty"})
+@CucumberOptions(features = "src/test/resources/features", plugin = {"pretty"})
 public class RunCucumber {
 }
--- src/test/java/selenium/TestBasicFormSteps.java
+++ src/test/java/selenium/TestBasicFormSteps.java
@@ -14,7 +14,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assume.assumeTrue;
 
-public class TestBasicForm {
+public class TestBasicFormSteps {
 
     WebDriver driver;
     String expected;
